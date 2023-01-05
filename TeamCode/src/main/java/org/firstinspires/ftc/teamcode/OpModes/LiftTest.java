b9572b516ba3e0632587671f967e92afe4b86622
G-ForceGarage <82465924+G-ForceGarage@users.noreply.github.com>
2023-01-05 17:04:46 -0600

    worked on lift PID (no major progress)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/LiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/LiftTest.java
@@ -24,8 +24,9 @@ import org.firstinspires.ftc.teamcode.Hardware.HWProfile;
 @TeleOp(name = "Lift Test", group = "Competition")
 //@Disabled
 public class LiftTest extends LinearOpMode {
-    public DcMotorEx motorLiftFront = null;
-    public DcMotorEx motorLiftRear = null;
+    public MotorEx motorLiftFront = null;
+    public MotorEx motorLiftRear = null;
+    public MotorGroup winch = null;
     FtcDashboard dashboard;
     //public MotorGroup winch = null;
     public static int LIFT_BOTTOM=0;
@@ -34,6 +35,8 @@ public class LiftTest extends LinearOpMode {
     public static int LIFT_HIGH=800;
     public static double LIFT_POW_DOWN=0.3;
     public static double LIFT_POW_UP=1;
+    public static double LIFT_KP=0.05;
+    public static int LIFT_TOL=10;
 
 
     @Override
@@ -48,31 +51,22 @@ public class LiftTest extends LinearOpMode {
         //lift motor init
         //motorLiftFront = new MotorEx(hardwareMap, "motorLiftFront", Motor.GoBILDA.RPM_1150);
         //motorLiftRear = new MotorEx(hardwareMap, "motorLiftRear", Motor.GoBILDA.RPM_1150);
-        motorLiftFront = hardwareMap.get(DcMotorEx.class, "motorLiftFront");
-        motorLiftRear = hardwareMap.get(DcMotorEx.class, "motorLiftRear");
-        motorLiftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorLiftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorLiftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorLiftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorLiftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorLiftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLiftFront = new MotorEx(hardwareMap,"motorLiftFront", Motor.GoBILDA.RPM_1150);
+        motorLiftRear = new MotorEx(hardwareMap,"motorLiftRear", Motor.GoBILDA.RPM_1150);
 
-
-        /*
         winch = new MotorGroup(motorLiftFront,motorLiftRear);
         winch.setRunMode(Motor.RunMode.PositionControl);
         winch.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         winch.setPositionCoefficient(LIFT_KP);
         winch.setPositionTolerance(LIFT_TOL);
         winch.resetEncoder();
-         */
+
 
         telemetry.addData("Ready to Run: ", "GOOD LUCK");
         telemetry.update();
 
         dashTelemetry.put("01 - Lift Target Position ", liftPos);
-        dashTelemetry.put("02 - Lift Actual Position", motorLiftFront.getCurrentPosition());
-        //dashTelemetry.put("02 - Lift Actual Position", winch.getPositions());
+        dashTelemetry.put("02 - Winch Motor Positions", winch.getPositions());
         dashTelemetry.put("03 - Lift Power = ", liftPower);
         dashTelemetry.put("04 - GP1.Button.A = ", "RESET LIFT");
         dashTelemetry.put("05 - GP1.Button.B = ", "LIFT LOW JUNCTION");
@@ -83,6 +77,9 @@ public class LiftTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            winch.setPositionTolerance(LIFT_TOL);
+            winch.setPositionCoefficient(LIFT_KP);
+
             if(gamepad1.a){
                 liftPos=LIFT_BOTTOM;
             }
@@ -99,7 +96,7 @@ public class LiftTest extends LinearOpMode {
                 liftPos=LIFT_HIGH;
             }
 
-            /*
+            winch.set(0);
             winch.setTargetPosition(liftPos);
             while(!winch.atTargetPosition()) {
 //                winch.setTargetPosition(liftPos);
@@ -109,29 +106,26 @@ public class LiftTest extends LinearOpMode {
                     winch.set(LIFT_POW_DOWN);
                 }
             }
-             */
-            motorLiftFront.setTargetPosition(liftPos);
-            motorLiftRear.setTargetPosition(liftPos);
-            if(liftPos>motorLiftFront.getCurrentPosition()){
-                liftPower=LIFT_POW_UP;
-            }else{
-                liftPower=LIFT_POW_DOWN;
-            }
-            motorLiftFront.setPower(1);
-            motorLiftRear.setPower(1);
+            winch.stopMotor();
 
             // Provide user feedback
             dashTelemetry.put("01 - Lift Target Position ", liftPos);
-            dashTelemetry.put("02 - Lift Front Position", motorLiftFront.getCurrentPosition());
-            dashTelemetry.put("02 - Lift Rear Position", motorLiftRear.getCurrentPosition());
-            dashTelemetry.put("03 - Lift Front Power = ", motorLiftFront.getPower());
-            dashTelemetry.put("03 - Lift Rear Power = ", motorLiftRear.getPower());
+            dashTelemetry.put("02 - Winch Motor Positions", winch.getPositions());
+            dashTelemetry.put("03 - Winch KP", winch.getPositionCoefficient());
             dashTelemetry.put("04 - GP1.Button.A = ", "RESET LIFT");
             dashTelemetry.put("05 - GP1.Button.B = ", "LIFT LOW JUNCTION");
             dashTelemetry.put("06 - GP1.Button.X = ", "LIFT MID JUNCTION");
             dashTelemetry.put("07 - GP1.Button.Y = ", "LIFT HIGH JUNCTION");
             dashboard.sendTelemetryPacket(dashTelemetry);
 
+            telemetry.addData("Lift Target Pos: ",liftPos);
+            telemetry.addData("Winch Motor Positions: ",winch.getPositions());
+            telemetry.addData("Winch KP: ",winch.getPositionCoefficient());
+            telemetry.addData("GP1.Button.A: ","RESET LIFT");
+            telemetry.addData("GP1.Button.B: ","LIFT LOW JUNCTION");
+            telemetry.addData("GP1.Button.X: ","LIFT MID JUNCTION");
+            telemetry.addData("GP1.Button.Y: ","LIFT HIGH JUNCTION");
+            telemetry.update();
         }   // end of while(opModeIsActive)
     }   // end of runOpMode()
 }       // end of LiftTest class
