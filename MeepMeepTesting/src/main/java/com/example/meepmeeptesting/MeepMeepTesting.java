40d62dec96960c316d6f3f2fa16ffcfe7db98e4e
G-ForceGarage <82465924+G-ForceGarage@users.noreply.github.com>
2023-02-21 13:52:24 -0600

    added NewBlueCycleLowAuto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,57 +17,35 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(45, 45, 17.927212540948645, Math.toRadians(360), 11.42)
+                .setConstraints(45, 65, 17.927212540948645, Math.toRadians(360), 11.42)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(35.25,-64, Math.toRadians(90)))
                                 //drive to mid pole
-                                .splineTo(new Vector2d(28.5,-31),Math.toRadians(120))
-                                .waitSeconds(0.25)
-                                .turn(Math.toRadians(-60))
-                                .splineToLinearHeading(new Pose2d(60,-12,Math.toRadians(0)),Math.toRadians(0))
-                                .waitSeconds(0.25)
-                                .back(6)
-                                .splineToSplineHeading(new Pose2d(30,-17.5,Math.toRadians(220)),Math.toRadians(190))
+                                .waitSeconds(0.125)
+                                .splineTo(new Vector2d(27,-29.5),Math.toRadians(120))
+                                .waitSeconds(0.35)
+                                .back(3)
+                                .turn(Math.toRadians(-45))
+                                .splineToLinearHeading(new Pose2d(55.5,-6,Math.toRadians(0)),Math.toRadians(0))
+                                .forward(6)
+                                .waitSeconds(0.35)
 
-                                .waitSeconds(0.25)
+                                //low cycle code
                                 .back(1)
-                                .splineToSplineHeading(new Pose2d(60,-12,Math.toRadians(0)),Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(36,-24),Math.toRadians(0))
+                                .forward(4)
+                                .waitSeconds(0.35)
+                                .back(2)
+                                .splineToConstantHeading(new Vector2d(55.5,-6),Math.toRadians(0))
+                                .forward(6)
 
-                                .waitSeconds(0.25)
-                                .back(6)
-                                .splineToSplineHeading(new Pose2d(30,-6.5,Math.toRadians(130)),Math.toRadians(170))
-
-                                .waitSeconds(0.25)
                                 .back(1)
-                                .splineToSplineHeading(new Pose2d(60,-12,Math.toRadians(0)),Math.toRadians(0))
-
-                                .waitSeconds(0.25)
-                                .back(6)
-                                .splineToSplineHeading(new Pose2d(30,-6.5,Math.toRadians(130)),Math.toRadians(170))
-
-                                .waitSeconds(0.25)
-                                .back(1)
-                                .splineToSplineHeading(new Pose2d(60,-12,Math.toRadians(0)),Math.toRadians(0))
-
-                                .waitSeconds(0.25)
-                                .back(6)
-                                .splineToSplineHeading(new Pose2d(30,-6.5,Math.toRadians(130)),Math.toRadians(170))
-
-                                .waitSeconds(0.25)
-                                .back(1)
-                                .splineToSplineHeading(new Pose2d(60,-12,Math.toRadians(0)),Math.toRadians(0))
-
-                                .waitSeconds(0.25)
-                                .back(6)
-                                .splineToSplineHeading(new Pose2d(30,-6.5,Math.toRadians(130)),Math.toRadians(170))
-
-                                .waitSeconds(0.25)
-                                .back(1)
-
-                                .splineToSplineHeading(new Pose2d(60,-12,Math.toRadians(90)),Math.toRadians(0))
-
-
-
+                                .splineToConstantHeading(new Vector2d(36,-24),Math.toRadians(0))
+                                .forward(4)
+                                .waitSeconds(0.35)
+                                .back(2)
+                                .splineToSplineHeading(new Pose2d(36,-14,Math.toRadians(90)),Math.toRadians(90))
+                                .strafeLeft(24)
 
                                 .build()
                 );
